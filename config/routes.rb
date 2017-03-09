dfc5aee349442b9ec73592d197411286aa6170f9
rails --version <return>
2017-03-09 16:45:31 -0600

    LucioMod

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'index/welcome'
+root 'index#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
