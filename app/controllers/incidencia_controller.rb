36dc8837794c82b9574561f7d9540e38b78140d7
Hector Esparza <hector.esparza@tibs.com.mx>
2017-03-10 14:09:19 -0600

    ProblemasRuta

--- app/controllers/incidencia_controller.rb
+++ app/controllers/incidencia_controller.rb
@@ -1,10 +1,10 @@
 class IncidenciaController < ApplicationController
-  before_action :set_incidencium, only: [:show, :edit, :update, :destroy]
+  before_action :set_incidencia, only: [:show, :edit, :update, :destroy]
 
   # GET /incidencia
   # GET /incidencia.json
   def index
-    @incidencia = Incidencium.all
+    @incidencia = Incidencia.all
   end
 
   # GET /incidencia/1
@@ -14,7 +14,7 @@ class IncidenciaController < ApplicationController
 
   # GET /incidencia/new
   def new
-    @incidencium = Incidencium.new
+    @Incidencia = Incidencia.new
   end
 
   # GET /incidencia/1/edit
@@ -24,15 +24,15 @@ class IncidenciaController < ApplicationController
   # POST /incidencia
   # POST /incidencia.json
   def create
-    @incidencium = Incidencium.new(incidencium_params)
+    @Incidencia = Incidencia.new(Incidencia_params)
 
     respond_to do |format|
-      if @incidencium.save
-        format.html { redirect_to @incidencium, notice: 'Incidencium was successfully created.' }
-        format.json { render :show, status: :created, location: @incidencium }
+      if @Incidencia.save
+        format.html { redirect_to @Incidencia, notice: 'Incidencia was successfully created.' }
+        format.json { render :show, status: :created, location: @Incidencia }
       else
         format.html { render :new }
-        format.json { render json: @incidencium.errors, status: :unprocessable_entity }
+        format.json { render json: @Incidencia.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -41,12 +41,12 @@ class IncidenciaController < ApplicationController
   # PATCH/PUT /incidencia/1.json
   def update
     respond_to do |format|
-      if @incidencium.update(incidencium_params)
-        format.html { redirect_to @incidencium, notice: 'Incidencium was successfully updated.' }
-        format.json { render :show, status: :ok, location: @incidencium }
+      if @Incidencia.update(Incidencia_params)
+        format.html { redirect_to @Incidencia, notice: 'Incidencia was successfully updated.' }
+        format.json { render :show, status: :ok, location: @Incidencia }
       else
         format.html { render :edit }
-        format.json { render json: @incidencium.errors, status: :unprocessable_entity }
+        format.json { render json: @Incidencia.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -54,21 +54,21 @@ class IncidenciaController < ApplicationController
   # DELETE /incidencia/1
   # DELETE /incidencia/1.json
   def destroy
-    @incidencium.destroy
+    @Incidencia.destroy
     respond_to do |format|
-      format.html { redirect_to incidencia_url, notice: 'Incidencium was successfully destroyed.' }
+      format.html { redirect_to incidencia_url, notice: 'Incidencia was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_incidencium
-      @incidencium = Incidencium.find(params[:id])
+    def set_Incidencia
+      @Incidencia = Incidencia.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def incidencium_params
-      params.require(:incidencium).permit(:EmpresaId, :RutaId, :Comentario, :Compartir, :RedSocial, :PostRed)
+    def Incidencia_params
+      params.require(:Incidencia).permit(:EmpresaId, :RutaId, :Comentario, :Compartir, :RedSocial, :PostRed)
     end
 end
