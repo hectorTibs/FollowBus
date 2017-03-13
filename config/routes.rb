ffd3f7b2c26ec5ecbeab055b56ba3e4877edd424
Hector Esparza <hector.esparza@tibs.com.mx>
2017-03-13 16:17:24 -0600

    Login

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   resources :reportes
   resources :camiones
-  #sget 'login/index'
+  get 'login/index'
 
-  #get 'maps/index'
+  get 'maps/index'
 
   resources :incidencia
   resources :ruta
   resources :redes_sociales
   resources :empresas
-  #get 'index/welcome'
-root 'home#index'
+  get 'index/welcome'
+root 'login#index'
 get '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], as: :finish_signup
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
