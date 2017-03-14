6fd0a803e348c046c797e41e044c725fede379e3
Hector Esparza <hector.esparza@tibs.com.mx>
2017-03-14 15:04:15 -0600

    Compartir

--- app/controllers/reportes_controller.rb
+++ app/controllers/reportes_controller.rb
@@ -11,6 +11,8 @@ class ReportesController < ApplicationController
   # GET /reportes/1.json
   def show
   end
+   def compartir
+  end
 
   # GET /reportes/new
   def new
@@ -29,7 +31,7 @@ class ReportesController < ApplicationController
     respond_to do |format|
       if @reporte.save
         format.html { redirect_to @reporte, notice: 'Reporte was successfully created.' }
-        format.json { render :show, status: :created, location: @reporte }
+        format.json { render :compartir, status: :created, location: @reporte }
       else
         format.html { render :new }
         format.json { render json: @reporte.errors, status: :unprocessable_entity }
