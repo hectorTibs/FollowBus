dd73b2b22a68d183c14ab42170850f61a7f7a698
Hector Esparza <hector.esparza@tibs.com.mx>
2017-03-16 12:04:36 -0600

    Compartir

--- config/routes.rb
+++ config/routes.rb
@@ -16,11 +16,7 @@ Rails.application.routes.draw do
   resources :empresas
   get 'index/welcome'
 root 'reportes#new'
-get '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], as: :finish_signup
-controller :posts do
-  get 'posts/show'     => :list
-  get 'posts/delete'   => :delete
-  get 'posts/edit/:id' => :edit
+#get '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], as: :finish_signup
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
