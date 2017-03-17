dc428646b0c1a1213d000fe9db0bebd9a6921dc6
Hector Esparza <hector.esparza@tibs.com.mx>
2017-03-17 11:05:54 -0600

    Compartir

--- vendor/assets/javascripts/fb.js
+++ vendor/assets/javascripts/fb.js
@@ -52,20 +52,37 @@ FB.login(function(){
   }
 });*/
 
-FB.api(
+/*FB.api(
     "/me/feed",
     "POST",
     {
         "message": "This is a test message",
         "place": "169983423516314",
-        "tags": "10153283482166868,1720852638142672"
+        "tags": "10153283482166868,1720852638142672",
+        "object_attachment":"http://s3.amazonaws.com/digitaltrends-uploads-prod/2016/08/Mustang-GT500.jpg"
     },
     function (response) {
       if (response && !response.error) {
-        console.log(response); /* post id will be returned */
+        console.log(response); 
       }
     }
-);
+);*/
+var params = {};
+params['message'] = 'Message';
+params['name'] = 'Name';
+params['description'] = 'Description';
+params['place'] = '733224400184471';
+params['picture'] = 'http://s3.amazonaws.com/digitaltrends-uploads-prod/2016/08/Mustang-GT500.jpg';
+params['caption'] = 'Caption';
+  
+FB.api('/me/feed', 'post', params, function(response) {
+  if (!response || response.error) {
+    alert('Error occured');
+  } else {
+    alert('Published to stream - you might want to delete it now!');
+  }
+});
+
 }, {scope: 'publish_actions'});
 
 }
