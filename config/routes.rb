1814cc084f9748d672a4d595e9934844b1831baf
Hector Esparza <hector.esparza@tibs.com.mx>
2017-03-21 09:15:11 -0600

    Seguridad

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 root 'reportes#new'
 
 devise_scope :user do
-  match '/sign-in' => "devise/sessions#new", :as => :login
+  get 'signin' => "devise/sessions#new", :as => :login
 end
 #get '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], as: :finish_signup
 
