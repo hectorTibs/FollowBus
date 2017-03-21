58d4b51a6992837655c26f457506f84fba741d1e
Hector Esparza <hector.esparza@tibs.com.mx>
2017-03-21 11:29:41 -0600

    Seguridad

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,4 +1,4 @@
-class Identity < ApplicationRecord
+class Identity < ActiveRecord::Base
    belongs_to :user
   validates_presence_of :uid, :provider
   validates_uniqueness_of :uid, :scope => :provider
