9806337c2cb6365ff93568a509d013088e303e67
Hector Esparza <hector.esparza@tibs.com.mx>
2017-03-21 11:35:44 -0600

    Seguridad

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,29 +1,34 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def self.provides_callback_for(provider)
-    class_eval %Q{
-      def #{provider}
-        @user = User.find_for_oauth(env["omniauth.auth"], current_user)
-
-        if @user.persisted?
-          sign_in_and_redirect @user, event: :authentication
-          set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
-        else
-          session["devise.#{provider}_data"] = env["omniauth.auth"]
-          redirect_to new_user_registration_url
-        end
-      end
-    }
-  end
-
-  [:twitter, :facebook, :linked_in].each do |provider|
-    provides_callback_for provider
-  end
-
-  def after_sign_in_path_for(resource)
-    if resource.email_verified?
-      super resource
+ def twitter
+    @user = User.find_for_oauth(env["omniauth.auth"], current_user)
+ 
+    if @user.persisted? # Chequea que nuestro usuario se haya guardado en la base de datos y no sea una instancia superficial
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "twitter".capitalize) if is_navigational_format?
     else
-      finish_signup_path(resource)
+      session["devise.twitter_data"] = env["omniauth.auth"]
+      redirect_to new_user_registration_url
     end
   end
+ 
+  def facebook
+    @user = User.find_for_oauth(env["omniauth.auth"], current_user)
+ 
+    if @user.persisted? # Chequea que nuestro usuario se haya guardado en la base de datos y no sea una instancia superficial
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "facebook".capitalize) if is_navigational_format?
+    else
+      session["devise.facebook_data"] = env["omniauth.auth"]
+     redirect_to new_user_registration_url
+    end
+  end
+ 
+  def after_sign_in_path_for(resource) # Revisa después de cada login si el mail del usuario es válido
+    if resource.email_verified?
+      super resource # Acción por defecto de Devise (si no está configurada, va al root_path)
+    else
+     finish_signup_path(resource)
+    end
+  end
+ 
 end
