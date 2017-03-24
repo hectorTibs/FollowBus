613cbb40deb831c373a5a929055fd7ce5320e833
Hector Esparza <hector.esparza@tibs.com.mx>
2017-03-24 10:59:08 -0600

    Seguridad

--- vendor/assets/javascripts/fb.js
+++ vendor/assets/javascripts/fb.js
@@ -44,16 +44,16 @@ FB.login(function(response){
   // Note: The call will only work if you accept the permission request
   if (response.authResponse) {
      var access_token =   FB.getAuthResponse()['accessToken'];
-     console.log('Access Token = '+ access_token);
+     alert('Access Token = '+ access_token);
      FB.api('/me?access_token='+access_token, function(response) {
-     console.log('Good to see you, ' + response.name + '.');
+     alert('Good to see you, ' + response.name + '.');
      });
    } else {
-     console.log('User cancelled login or did not fully authorize.');
+     alert('User cancelled login or did not fully authorize.');
    }
 
   var body =  $("#post_comentario").val();
-FB.api('/followbusMx/feed?access_token=EAAKa3Rvp5JcBADP6zlhkDu7wgYPEg6sM6bZA6JtINV0l4DnnZAnhKZBMlloOsym5mjE1RhImGHRcfWoy0ilODCIDiiPMv1QoBzKuFteFVhCnZAaEymLPLrLmaiqNjuABgYrwd5Oulfpx1UKiBFBWEOKmj7edo5kgrhmOoMZAFb2BWuBlCQaPU', 'post',{
+/*FB.api('/followbusMx/feed?access_token=EAAKa3Rvp5JcBADP6zlhkDu7wgYPEg6sM6bZA6JtINV0l4DnnZAnhKZBMlloOsym5mjE1RhImGHRcfWoy0ilODCIDiiPMv1QoBzKuFteFVhCnZAaEymLPLrLmaiqNjuABgYrwd5Oulfpx1UKiBFBWEOKmj7edo5kgrhmOoMZAFb2BWuBlCQaPU', 'post',{
         "message": body,
         //"place": "1720852638142672",
   
@@ -70,7 +70,7 @@ FB.api('/followbusMx/feed?access_token=EAAKa3Rvp5JcBADP6zlhkDu7wgYPEg6sM6bZA6JtI
     setTimeout(function(){ $('#shareM').modal('hide'); }, 3000);
   }
 });
-
+*/
 
 }, {scope: 'publish_actions'});
 
