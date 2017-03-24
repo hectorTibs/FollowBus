e48e4c1bdeb55b701daf28991ac755eb48499558
Hector Esparza <hector.esparza@tibs.com.mx>
2017-03-24 09:25:40 -0600

    Seguridad

--- vendor/assets/javascripts/fb.js
+++ vendor/assets/javascripts/fb.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-     $('#fb-I').attr( "src", "/assets/Face_activo-f6770488e3cace3a8010291d39ee76dedf482bd4f857f88d78e912a1365d1899.svg" );
+     $('.fb-I').attr( "src", "/assets/Face_activo-f6770488e3cace3a8010291d39ee76dedf482bd4f857f88d78e912a1365d1899.svg" );
    
   alert("Entro")
 });
