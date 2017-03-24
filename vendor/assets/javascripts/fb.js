4c9f80fd4f7519abd04689a5d7b7164b9cd8c767
Hector Esparza <hector.esparza@tibs.com.mx>
2017-03-24 09:32:27 -0600

    Seguridad

--- vendor/assets/javascripts/fb.js
+++ vendor/assets/javascripts/fb.js
@@ -69,21 +69,3 @@ FB.api('/followbusMx/feed?access_token=EAAKa3Rvp5JcBAF4aTeNpBrg7ZBYczH2nTMsMyiLa
    }(document, 'script', 'facebook-jssdk'));
 
   /*Facebook Login status*/
-
-
-
-
-
-
-
-
-
-
-
-
-$( document ).ready(function() {
-    console.log( "ready!" );
-     $('.fb-I').attr( "src", "/assets/Face_activo-f6770488e3cace3a8010291d39ee76dedf482bd4f857f88d78e912a1365d1899.svg" );
-   
-  alert("Entro")
-});
