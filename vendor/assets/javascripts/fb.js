f0b48c71462d6ffa0db831b028dfc5887988357e
Hector Esparza <hector.esparza@tibs.com.mx>
2017-03-27 16:07:02 -0600

    Seguridad

--- vendor/assets/javascripts/fb.js
+++ vendor/assets/javascripts/fb.js
@@ -61,7 +61,7 @@ FB.api('/me/feed?access_token='+access_token, 'post',{
   
         //"tags":"AaJ0zF2vGsFX0xAu6xjEiLxENzPohC1g2WJRoacUzZuClPcaTfOEKR_1btTMn6hEhdZ-0rLyI3W4R8WjCFWelwGgatqkBgFhMSIGxXUKrIjlUQ" ,
        // "object_attachment":"http://s3.amazonaws.com/digitaltrends-uploads-prod/2016/08/Mustang-GT500.jpg",
-      //  "privacy":{"value":"SELF"}
+        "privacy":{"value":"SELF"}
      //  "privacy":{"value":"CUSTOM","allow":"1720852638142672"}
       
     }, function(response) {
